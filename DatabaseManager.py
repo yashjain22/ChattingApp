cfec35c35d8ff37047c5f21cbaf49f7486c592f7
yashjain22 <33537695+yashjain22@users.noreply.github.com>
2018-04-15 10:58:47 +0530

    Add files via upload

---
+++ DatabaseManager.py
@@ -0,0 +1,127 @@
+class DatabaseManager:
+    
+    def login(self,mysql, username, password):
+        cur = mysql.connection.cursor()
+        sql_query = '''SELECT user_id, username, first_name, last_name FROM users WHERE 
+            username = %s AND password = %s'''
+        cur.execute(sql_query, [username, password])
+        result = list(cur.fetchall())
+        cur.close() 
+        return result
+
+    def register(self,mysql, user):
+        cur = mysql.connection.cursor()
+        sql_query = '''INSERT INTO users SET username = %s, password = %s, 
+            first_name = %s, last_name = %s'''
+        cur.execute(sql_query, [user['username'], user['password'], user['first_name'], user['last_name']])
+        mysql.connection.commit()
+    
+    def search(self,mysql, user_id, user_query):
+        cur = mysql.connection.cursor()
+        sql_query = '''SELECT username, first_name, last_name FROM users WHERE 
+            (first_name LIKE %s OR last_name LIKE %s OR username LIKE %s) AND NOT user_id = %s ORDER BY 
+            first_name, last_name'''
+        cur.execute(sql_query, [user_query + '%', user_query + '%', user_query + '%', user_id])
+        results = list(cur.fetchall())
+        print(results)
+        for result in results:
+            result['is_friends'] = False
+            if(self.is_friends(mysql, user_id, result['username'])):
+                result['is_friends'] = True
+        cur.close()
+        print(results)
+        return results
+
+    def get_user_id(self,mysql,username):
+        cur = mysql.connection.cursor()
+        sql_query = 'SELECT user_id FROM users WHERE username = %s'
+        cur.execute(sql_query, [username])
+        result = list(cur.fetchall())
+        return result[0]['user_id']
+
+    def add_contact(self,mysql, user_id, username):
+        cur = mysql.connection.cursor()
+        other_contact = self.get_user_id(mysql,username)
+        sql_query = 'INSERT INTO friends_list SET user_1 = %s, user_2 = %s'
+        cur.execute(sql_query, [user_id, other_contact])
+        mysql.connection.commit()
+
+    def is_friends(self,mysql, user_id, username):
+        cur = mysql.connection.cursor()
+        other_contact = self.get_user_id(mysql, username)
+        sql_query = '''SELECT friend_id FROM friends_list 
+            WHERE (user_1 = %s AND user_2 = %s) OR (user_1 = %s AND user_2 = %s)'''
+        cur.execute(sql_query, [user_id, other_contact, other_contact, user_id])
+        result = list(cur.fetchall())
+        return len(result) > 0
+
+    def get_contacts(self,mysql, user_id):
+        cur = mysql.connection.cursor()
+        sql_query = '''SELECT username, CONCAT_WS(' ', first_name, last_name) as contact_name FROM users WHERE 
+            user_id IN (SELECT user_1 FROM friends_list WHERE user_2 = %s) OR 
+            user_id IN(SELECT user_2 FROM friends_list WHERE user_1 = %s)'''
+        cur.execute(sql_query, [user_id, user_id])
+        user = list(cur.fetchall())
+        return user
+
+    def get_user(self,mysql, username):
+        cur = mysql.connection.cursor()
+        sql_query = '''SELECT first_name, last_name FROM users WHERE username = %s'''
+        cur.execute(sql_query, [username])
+        user = list(cur.fetchall())
+        return user[0]
+
+    def get_last_message(self,mysql, user_id, username):
+        cur = mysql.connection.cursor()
+        sql_query = '''SELECT message FROM messages WHERE'''
+
+    def send_message(self,mysql, message, sender, recipient):
+        cur = mysql.connection.cursor()
+        other_contact = self.get_user_id(mysql, recipient)
+        user_id = self.get_user_id(mysql, sender)
+        sql_query = '''INSERT INTO messages SET message = %s, sender_id = %s, recipient_id = %s'''
+        cur.execute(sql_query, [message, user_id, other_contact])
+        mysql.connection.commit()
+
+    def get_messages(self,mysql, user_id, username):
+        cur = mysql.connection.cursor()
+        other_contact = self.get_user_id(mysql, username)
+        sql_query = '''SELECT message, created_on, sender_id,
+            (SELECT username FROM users WHERE user_id = sender_id) as sender, 
+            (SELECT username FROM users WHERE user_id = recipient_id) as recipient FROM messages 
+            WHERE (sender_id = %s AND recipient_id = %s) OR
+            (recipient_id = %s AND sender_id = %s) ORDER BY created_on'''
+        cur.execute(sql_query, [user_id, other_contact, user_id, other_contact])
+        messages = list(cur.fetchall()) 
+        return messages
+
+    def save_file(self,mysql, file_info, sender, recipient):
+        cur = mysql.connection.cursor()
+        other_contact = self.get_user_id(mysql, recipient)
+        sql_query = '''INSERT INTO files (file_name, file_path, sender_id, recipient_id) 
+            VALUES (%s, %s, %s, %s)'''
+        print(sql_query)
+        file_name = file_info['old_filename'] + file_info['file_ext']
+        file_path = file_info['new_filename'] + file_info['file_ext']
+        cur.execute(sql_query, [file_name, file_path, sender, other_contact])
+        mysql.connection.commit()
+
+    def get_files(self,mysql, user_id, username):
+        cur = mysql.connection.cursor()
+        other_contact = self.get_user_id(mysql, username)
+        sql_query = '''SELECT file_id, file_name, created_on, 
+            (SELECT username FROM users WHERE user_id = sender_id) as sender, 
+            (SELECT username FROM users WHERE user_id = recipient_id) as recipient FROM files 
+            WHERE (sender_id = %s AND recipient_id = %s) OR
+            (recipient_id = %s AND sender_id = %s) ORDER BY created_on'''
+        cur.execute(sql_query, [user_id, other_contact, user_id, other_contact])
+        files = list(cur.fetchall())
+        return files
+
+    def get_file(self,mysql, file_id):
+        cur = cur = mysql.connection.cursor()
+        sql_query = '''SELECT file_name, file_path FROM files WHERE file_id = %s'''
+        cur.execute(sql_query, [file_id])
+        new_file = list(cur.fetchall())
+        print(new_file)
+        return new_file[0]
---
+++ app.py
@@ -0,0 +1,194 @@
+from flask import Flask , session, render_template, request, redirect, g, url_for, jsonify, send_file
+from flask_mysqldb import MySQL
+from flask_socketio import SocketIO, emit
+from DatabaseManager import DatabaseManager
+import json, os, random, datetime
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secretparabibo'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'test'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+database=DatabaseManager()
+mysql = MySQL(app)
+socketio = SocketIO(app)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+online = {
+}
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+    else:
+        session['user'] = None
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title='Page not Found')
+
+@app.route('/messages/')
+@app.route('/messages/<username>')
+def messages(username=None):
+    contacts = database.get_contacts(mysql, g.user['user_id'])
+    if username == None and len(contacts) > 0:
+        username = contacts[0]['username']
+    elif len(contacts) < 1:
+        return render_template('index.html', 
+            title='Message',
+            contacts=contacts,
+            user=g.user['first_name'],
+            active_contact='No Friends yet',
+            messages='',
+            files_shared='')
+    active_contact = database.get_user(mysql, username)
+    messages = database.get_messages(mysql, g.user['user_id'], username)
+    files = database.get_files(mysql, g.user['user_id'], username)
+    files = shorten_filename(files)
+    return render_template('index.html', 
+        title='Message',
+        contacts=contacts,
+        user=g.user,
+        active_contact={'name': active_contact['first_name'] + ' ' + active_contact['last_name'], 
+            'username': username},
+        messages=messages,
+        files_shared=files)
+
+def shorten_filename(files):
+    for _file in files:
+        new_file = convert_file_name(_file['file_name'])
+        file_name = new_file['old_filename']
+        if(len(file_name) > 15):
+            file_name = new_file['old_filename'][:13]
+            file_name = '{}...'.format(file_name)
+        file_name = '{}.{}'.format(file_name, new_file['file_ext'])
+        _file['file_name'] = file_name
+    
+    return files
+
+@app.route('/')
+def index():
+    if session['user'] == None:
+        return render_template('login.html')
+    return redirect(url_for('messages'))
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/server/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('./')
+
+@app.route('/server/login', methods=['POST'])
+def server_login():
+    username = request.form['username']
+    password = request.form['password']
+    result = database.login(mysql, username, password)
+    if len(result) > 0:
+        session['user'] = result[0]
+    return redirect('/')
+
+@app.route('/server/register', methods=['POST'])
+def server_register():
+    user = request.form['register']
+    database.register(mysql, user)
+    return redirect('/')
+
+@app.route('/server/search', methods=['POST'])
+def server_search():
+    user_query = request.form['query']
+    result = jsonify(database.search(mysql, g.user['user_id'], user_query))
+    return result
+
+@app.route('/server/add_contact', methods=['POST'])
+def server_add_contact():
+    username = request.form['username']
+    database.add_contact(mysql, g.user['user_id'], username)
+    return "0"
+
+@app.route('/server/upload', methods=['POST'])
+def server_upload():
+
+    sender = g.user['user_id']
+    recipient = request.form['recipient']
+    sent_file = request.files['fileInput']
+    save_file(sent_file, sender, recipient)
+
+    return redirect('./messages/{}'.format(recipient))
+
+def save_file(sent_file, sender, recipient):
+    relative_path = 'static\\shared_files\\'
+    target = os.path.join(APP_ROOT, relative_path)
+    
+    if(sent_file != ''):
+        new_filename = convert_file_name(sent_file.filename)
+
+        database.save_file(mysql, new_filename, sender, recipient)
+        destination = '{}{}{}'.format(
+            target,
+            new_filename['new_filename'], 
+            new_filename['file_ext']
+        )
+        print('destination: ', destination)
+        sent_file.save(destination)
+
+def convert_file_name(filename):
+    rand_no = random.randint(100, 1001)
+    filename, extension = os.path.splitext(filename)
+    date = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    new_filename = "{}-{}".format(rand_no, date)
+    new_file = {'old_filename': filename, 
+        'new_filename': new_filename, 
+        'file_ext': extension
+    }
+    return new_file
+
+@app.route('/server/download')
+def server_download():
+    relative_path = 'static\\shared_files\\'
+    target = os.path.join(APP_ROOT, relative_path)
+    file_id = request.args['file_id']
+    selected_file = database.get_file(mysql, file_id)
+    return send_file('/'.join([target, selected_file['file_path']]), 
+        attachment_filename = selected_file['file_name'], as_attachment=True)
+
+# listen on event new message
+@socketio.on('new message')
+def handle_message(msg):
+    if(msg['recipient'] in online):
+        recipient_sid = online[msg['recipient']]
+        socketio.emit('send message', msg, room=recipient_sid)        
+        
+    database.send_message(mysql, 
+        msg['message'], 
+        msg['sender'], 
+        msg['recipient']
+    )
+    return msg
+
+@socketio.on('login')
+def handle_online(data):
+    data = json.loads(data)
+    online[data['username']] = request.sid
+    print(online)
+    return data['username']
+
+@socketio.on('update username sid')
+def update_username_sid(username):
+    online[username] = request.sid
+    print('update username sid', online)
+
+@socketio.on('logout')
+def handle_logout(username):
+    online.pop(username)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
