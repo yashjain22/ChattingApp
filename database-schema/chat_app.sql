3fbd0caaa69ddc111a51adb41391bea12d1c1a83
yashjain22 <33537695+yashjain22@users.noreply.github.com>
2018-07-23 00:04:33 +0530

    Update chat_app.sql

--- database-schema/chat_app.sql
+++ database-schema/chat_app.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 07, 2018 at 07:01 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.1.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
